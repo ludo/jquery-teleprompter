6f2d2a98fff17f8b801bc85cfaf4475e0935c671
Tomas Sevillano <tsevillano@radiomitre.com.ar>
2015-08-13 15:36:45 -0300

    avoid start queue

--- jquery.teleprompter.js
+++ jquery.teleprompter.js
@@ -1,5 +1,5 @@
 (function($) {
-  $.fn.teleprompter = function() {
+  $.fn.teleprompter = function(a) {
     return this.each(function() {
       var container = $(this),
           containerHeight,
@@ -12,13 +12,21 @@
       script.style.marginTop = containerHeight + "px";
 
       maxScrollTop = this.scrollHeight - containerHeight;
-
-      container.animate(
-        { scrollTop: maxScrollTop },
-        maxScrollTop * 10, // duration
-        "linear" // easing
-      );
+      console.log(a);
+      if (a == 'start'){
+        if (!$(container).is(':animated')) {
+          container.animate(
+          { scrollTop: maxScrollTop },
+          maxScrollTop * 25, // duration
+          "linear" // easing
+        );
+        }
+      }else if(a == 'pause'){
+        if ($(container).is(':animated')) {
+          container.stop();
+      }
+      }  
+      
     });
   };
 }(jQuery));
-
